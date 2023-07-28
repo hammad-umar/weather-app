2d9d56b351387f4d9a85ec9cd2546c48aa94d954
Hammad Umar <hammadhawc227@gmail.com>
2023-07-29 02:21:15 +0500

    style overrides for mui input label and input field

--- src/app/components/ThemeRegistry/theme.ts
+++ src/app/components/ThemeRegistry/theme.ts
@@ -1,5 +1,7 @@
 import { Roboto } from 'next/font/google'
 import { createTheme } from '@mui/material/styles'
+import { COLORS } from '@/constants/colors'
+import { colors } from '@mui/material'
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -22,6 +24,36 @@ const theme = createTheme({
             backgroundColor: '#60a5fa',
           }),
         }),
+      },
+    },
+
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: `${COLORS.WHITE_COLOR}`,
+          fontSize: `1rem`,
+        },
+      },
+    },
+
+    MuiInput: {
+      styleOverrides: {
+        root: {
+          fontSize: `1rem`,
+          fontWeight: 400,
+          color: `#f3f3f3`,
+          '&.MuiInput-underline:after': {
+            borderBottom: `2px solid ${colors.blue[700]}`,
+          },
+          '&.MuiInput-underline:hover:before': {
+            borderBottom: `2px solid ${COLORS.WHITE_COLOR}`,
+          },
+        },
+        underline: {
+          '&:before': {
+            borderBottom: `2px solid ${COLORS.WHITE_COLOR}`,
+          },
+        },
       },
     },
   },
